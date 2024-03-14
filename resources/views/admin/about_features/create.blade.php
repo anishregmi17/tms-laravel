1a92b01c6cc755fac50ccb02f64ebb9fb849ba98
Anish Regmi <103883548+anishregmi17@users.noreply.github.com>
2024-03-14 20:47:53 +0545

    Update create.blade.php

--- resources/views/admin/about_features/create.blade.php
+++ resources/views/admin/about_features/create.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-    <title>Title</title>
+    <title>About Features</title>
 </head>
 
 <body>
@@ -24,6 +24,13 @@
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                             <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Services</a>
+                        </li>
+                             </li>
+                             <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">About</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="#">Link</a>
